83b03d9039dc397fff0b2c5858ce970b2467c3cc
Ernesto Reyes Alvarez <47167966+Thetoro@users.noreply.github.com>
2019-02-05 22:07:30 -0800

    Add files via upload

---
+++ Presentacion 1/Diagnostico 2.cpp
@@ -0,0 +1,12 @@
+//Diagnostico 2
+
+#include <stdio.h>
+
+main(){
+	int r=50, s=100, b=6000, a;
+	
+	a=r*s;
+	
+	printf("El numero de personas sentadas es %d\n", a);
+	printf("El numero de personas paradas es de %d", b-a);
+}
